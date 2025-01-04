b46bf94723de930ead7412e7299a89b4ed0a5d05
J0onYEong <cjy2134@naver.com>
2025-01-04 18:21:45 +0900

    Feat, StageIndexView 구현

---
+++ Projects/Feature/Onboarding/Feature/Sources/InputWakeUpAlarm/View/StageIndexView.swift
@@ -0,0 +1,60 @@
+//
+//  StageIndexView.swift
+//  FeatureOnboarding
+//
+//  Created by choijunios on 1/4/25.
+//
+
+import FeatureResources
+
+import UIKit
+
+class StageIndexView: UIView {
+    
+    private let indexLabel: UILabel = .init()
+    
+    override var intrinsicContentSize: CGSize {
+        CGSize(width: 54, height: 30)
+    }
+    
+    
+    init() {
+        super.init(frame: .zero)
+        
+        setupLayout()
+    }
+    required init?(coder: NSCoder) { nil }
+    
+    
+    public func update(currentStage: Int, stageCount: Int) {
+        
+        // indexLabel
+        indexLabel.displayText = "\(currentStage)/\(stageCount)"
+            .displayText(font: .body1Medium, color: R.Color.gray200)
+    }
+    
+    
+    private func setupLayout() {
+        
+        // self
+        backgroundColor = R.Color.gray700
+        layer.cornerRadius = 10
+        
+        
+        // indexLabel
+        addSubview(indexLabel)
+        indexLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+}
+
+
+#Preview {
+    
+    let view = StageIndexView()
+    view.update(currentStage: 1, stageCount: 6)
+    
+    return view
+}
+
