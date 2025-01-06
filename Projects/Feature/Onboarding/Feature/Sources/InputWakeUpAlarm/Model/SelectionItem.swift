b40b1005cfb288276c005a6719ec14a88646666c
J0onYEong <cjy2134@naver.com>
2025-01-06 17:08:46 +0900

    Feat, PickerItemView및 모델 수정

---
+++ Projects/Feature/Onboarding/Feature/Sources/InputWakeUpAlarm/Model/SelectionItem.swift
@@ -0,0 +1,21 @@
+//
+//  SelectionItem.swift
+//  FeatureOnboarding
+//
+//  Created by choijunios on 1/4/25.
+//
+
+import UIKit
+
+struct SelectionItem {
+    
+    let content: String
+    let displayingText: String
+    let contentSize: CGSize
+    
+    init(content: String, displayingText: String, contentSize: CGSize) {
+        self.displayingText = displayingText
+        self.content = content
+        self.contentSize = contentSize
+    }
+}
