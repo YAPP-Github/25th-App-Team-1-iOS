3540a25e69523ee1513c291cef71145e9cb5b059
J0onYEong <cjy2134@naver.com>
2025-01-20 21:16:24 +0900

    Feat, 배경화면 구현

--- Projects/Feature/AlarmMission/Feature/Sources/ShakeMission/Working/ShakeMissionWorkingViewController.swift
+++ Projects/Feature/AlarmMission/Feature/Sources/ShakeMission/Working/ShakeMissionWorkingViewController.swift
@@ -15,7 +15,29 @@ protocol ShakeMissionWorkingPresentableListener: AnyObject {
     // interactor class.
 }
 
-final class ShakeMissionWorkingViewController: UIViewController, ShakeMissionWorkingPresentable, ShakeMissionWorkingViewControllable {
+final class ShakeMissionWorkingViewController: UIViewController, ShakeMissionWorkingPresentable, ShakeMissionWorkingViewControllable, ShakeMissionWorkingViewListener {
 
+    private(set) var mainView: ShakeMissionWorkingView!
+    
     weak var listener: ShakeMissionWorkingPresentableListener?
+    
+    override func loadView() {
+        let mainView = ShakeMissionWorkingView()
+        self.mainView = mainView
+        self.view = mainView
+        mainView.listener = self
+    }
+}
+
+
+// MARK: ShakeMissionWorkingViewListener
+extension ShakeMissionWorkingViewController {
+    
+    func action(_ action: ShakeMissionWorkingView.Action) {
+        
+    }
+}
+
+#Preview {
+    ShakeMissionWorkingViewController()
 }
---
+++ Projects/Feature/AlarmMission/Feature/Sources/ShakeMission/Working/Views/ShakeMissionWorkingView.swift
@@ -0,0 +1,96 @@
+//
+//  ShakeMissionWorkingView.swift
+//  AlarmMission
+//
+//  Created by choijunios on 1/20/25.
+//
+
+import UIKit
+
+import FeatureResources
+
+protocol ShakeMissionWorkingViewListener: AnyObject {
+    
+    func action(_ action: ShakeMissionWorkingView.Action)
+}
+
+class ShakeMissionWorkingView: UIView {
+    
+    // Action
+    enum Action {
+        
+    }
+    
+    // Listener
+    weak var listener: ShakeMissionWorkingViewListener?
+    
+    
+    // Sub views
+    
+    // - Background
+    private var backgroundLayer: CAGradientLayer = .init()
+    private let backgroundStar1: UIImageView = .init().then {
+        $0.image = FeatureResourcesAsset.shakeMissionStar1.image
+    }
+    private let backgroundStar2: UIImageView = .init().then {
+        $0.image = FeatureResourcesAsset.shakeMissionStar2.image
+    }
+    private let backgroundStar3: UIImageView = .init().then {
+        $0.image = FeatureResourcesAsset.shakeMissionStar1.image
+    }
+    
+    
+    init() {
+        super.init(frame: .zero)
+        setupUI()
+        setupLayout()
+    }
+    required init?(coder: NSCoder) { nil }
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        backgroundLayer.frame = self.layer.bounds
+    }
+}
+
+
+// MARK: Setup
+private extension ShakeMissionWorkingView {
+    
+    func setupUI() {
+        
+        // backgroundLayer
+        backgroundLayer.colors = [
+            UIColor(hex: "#1E3B68").cgColor,
+            UIColor(hex: "#3F5F8D").cgColor,
+        ]
+        backgroundLayer.startPoint = .init(x: 0.5, y: 0.0)
+        backgroundLayer.endPoint = .init(x: 0.5, y: 1.0)
+        self.layer.addSublayer(backgroundLayer)
+        
+        
+        // backgroundStars
+        [backgroundStar1,backgroundStar2,backgroundStar3]
+            .forEach({ self.addSubview($0) })
+        
+    }
+    
+    func setupLayout() {
+        
+        // backgroundStars
+        backgroundStar1.snp.makeConstraints { make in
+            make.left.equalToSuperview().inset(39)
+            make.bottom.equalTo(self.safeAreaLayoutGuide).inset(103)
+        }
+        backgroundStar2.snp.makeConstraints { make in
+            make.right.equalToSuperview()
+            make.bottom.equalTo(self.safeAreaLayoutGuide).inset(19.5)
+        }
+        backgroundStar3.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(2.4)
+            make.bottom.equalToSuperview().inset(6.8)
+        }
+    }
+}
