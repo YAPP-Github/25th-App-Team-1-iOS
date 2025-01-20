2830c951a975dea8bb64e1ee970b26557dd25e24
J0onYEong <cjy2134@naver.com>
2025-01-20 14:25:51 +0900

    Feat, 배경화면UI 구현

--- Projects/Feature/AlarmMission/Feature/Sources/ShakeMission/Main/Views/ShakeMissionMainView.swift
+++ Projects/Feature/AlarmMission/Feature/Sources/ShakeMission/Main/Views/ShakeMissionMainView.swift
@@ -7,6 +7,11 @@
 
 import UIKit
 
+import FeatureDesignSystem
+import FeatureResources
+
+import SnapKit
+
 protocol ShakeMissionMainViewListener: AnyObject {
     
     func action(_ action: ShakeMissionMainView.Action)
@@ -24,8 +29,79 @@ class ShakeMissionMainView: UIView {
     weak var listener: ShakeMissionMainViewListener?
     
     
+    // Sub views
+    
+    // - Background
+    private var backgroundLayer: CAGradientLayer = .init()
+    private let backgroundStar1: UIImageView = .init().then {
+        $0.image = FeatureResourcesAsset.shakeMissionStar1.image
+    }
+    private let backgroundStar2: UIImageView = .init().then {
+        $0.image = FeatureResourcesAsset.shakeMissionStar2.image
+    }
+    private let backgroundStar3: UIImageView = .init().then {
+        $0.image = FeatureResourcesAsset.shakeMissionStar1.image
+    }
+    
+    
     init() {
         super.init(frame: .zero)
+        setupUI()
+        setupLayout()
     }
     required init?(coder: NSCoder) { nil }
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        backgroundLayer.frame = self.layer.bounds
+    }
+}
+
+
+// MARK: Setup
+private extension ShakeMissionMainView {
+    
+    func setupUI() {
+        
+        // backgroundLayer
+        backgroundLayer.colors = [
+            UIColor(hex: "#1E3B68").cgColor,
+            UIColor(hex: "#3F5F8D").cgColor,
+        ]
+        backgroundLayer.startPoint = .init(x: 0.5, y: 0.0)
+        backgroundLayer.endPoint = .init(x: 0.5, y: 1.0)
+        self.layer.addSublayer(backgroundLayer)
+        
+        
+        // backgroundStars
+        [backgroundStar1,backgroundStar2,backgroundStar3]
+            .forEach({ self.addSubview($0) })
+        
+        
+        //
+    }
+    
+    func setupLayout() {
+        
+        // backgroundStars
+        backgroundStar1.snp.makeConstraints { make in
+            make.left.equalToSuperview().inset(39)
+            make.top.equalTo(self.safeAreaLayoutGuide).inset(20)
+        }
+        backgroundStar2.snp.makeConstraints { make in
+            make.right.equalToSuperview()
+            make.top.equalTo(self.safeAreaLayoutGuide).inset(82)
+        }
+        backgroundStar3.snp.makeConstraints { make in
+            make.left.equalToSuperview().inset(-2.5)
+            make.top.equalTo(self.safeAreaLayoutGuide).inset(148)
+        }
+    }
+}
+
+
+#Preview {
+    ShakeMissionMainView()
 }
