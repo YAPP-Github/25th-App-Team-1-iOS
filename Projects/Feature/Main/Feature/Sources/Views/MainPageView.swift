65bddde46ade2b0d85618d7e98b60de37626b191
J0onYEong <cjy2134@naver.com>
2025-01-27 21:51:21 +0900

    Feat, 언덕 UI 초기버전 구현

--- Projects/Feature/Main/Feature/Sources/Views/MainPageView.swift
+++ Projects/Feature/Main/Feature/Sources/Views/MainPageView.swift
@@ -9,6 +9,8 @@ import UIKit
 
 import FeatureResources
 
+import SnapKit
+
 protocol MainPageViewListener: AnyObject {
     
     func action(_ action: MainPageView.Action)
@@ -31,6 +33,8 @@ final class MainPageView: UIView {
     // Sub views
     // - Background
     private var backgroudCloudLayer: CALayer?
+    private let hillView = UIView()
+    
     
     
     init() {
@@ -46,6 +50,10 @@ final class MainPageView: UIView {
         if backgroudCloudLayer == nil {
             setupBackgroundLayout()
         }
+        
+        if hillView.layer.sublayers == nil {
+            setupHillView(ridgeHeight: 50)
+        }
     }
 }
 
@@ -58,25 +66,38 @@ private extension MainPageView {
         // self
         self.layer.backgroundColor = UIColor(hex: "#1F3B64").cgColor
         
+        // hillView
+        addSubview(hillView)
+        
     }
     
     
     func setupLayout() {
         
         // Background
-        // - backgroudCloudLayer
         
+        // hillView
+        hillView.snp.makeConstraints { make in
+            make.top.equalTo(self.safeAreaLayoutGuide).inset(166)
+            make.bottom.equalToSuperview()
+            make.horizontalEdges.equalToSuperview()
+        }
     }
-    
+}
+
+
+// MARK: Backgroud
+private extension MainPageView {
     
     func setupBackgroundLayout() {
         // Background cloud
+        // - setupUI
         let backgroudCloudLayer = CALayer()
         backgroudCloudLayer.contents = FeatureResourcesAsset.mainPageBackSkyCloud.image.cgImage
         backgroudCloudLayer.contentsGravity = .resizeAspect
         self.layer.addSublayer(backgroudCloudLayer)
+        // - setupLayout
         let imageSize = FeatureResourcesAsset.mainPageBackSkyCloud.image.size
-        
         let imageRatio = imageSize.height / imageSize.width
         let screenSize = self.layer.bounds
         let preferedImageWidth = screenSize.width * 1.3
@@ -91,8 +112,42 @@ private extension MainPageView {
         )
         self.backgroudCloudLayer = backgroudCloudLayer
     }
+    
+    
+    func setupHillView(ridgeHeight: CGFloat) {
+        // Shape layer
+        let shapeLayer = CAShapeLayer()
+        let hillWidth = hillView.bounds.width
+        let hillHeight = hillView.bounds.height
+        let cgPath = CGMutablePath()
+        cgPath.move(to: .init(x: 0, y: ridgeHeight))
+        cgPath.addQuadCurve(
+            to: .init(x: hillWidth/2, y: 0),
+            control: .init(x: 0, y: 0)
+        )
+        cgPath.addQuadCurve(
+            to: .init(x: hillWidth, y: ridgeHeight),
+            control: .init(x: hillWidth, y: 0)
+        )
+        cgPath.addLine(to: .init(x: hillWidth, y: hillHeight))
+        cgPath.addLine(to: .init(x: 0, y: hillHeight))
+        cgPath.closeSubpath()
+        shapeLayer.path = cgPath
+        
+        // Gradient layer
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor(hex: "#172E51").cgColor,
+            UIColor(hex: "#184385").cgColor,
+        ]
+        gradientLayer.startPoint = .init(x: 0.5, y: 0.0)
+        gradientLayer.endPoint = .init(x: 0.5, y: 1.0)
+        gradientLayer.mask = shapeLayer
+        hillView.layer.addSublayer(gradientLayer)
+        gradientLayer.frame = hillView.layer.bounds
+    }
 }
-
+ 
 
 #Preview {
     MainPageView()
