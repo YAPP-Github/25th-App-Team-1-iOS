62a1a87f13be4816d4e01cc7633968bc81db438a
J0onYEong <cjy2134@naver.com>
2025-01-29 22:42:13 +0900

    Feat, 드래그를 하는 Dock UI뷰 구현

---
+++ Projects/Feature/Main/Feature/Sources/Views/Components/DockView.swift
@@ -0,0 +1,53 @@
+//
+//  DockView.swift
+//  Main
+//
+//  Created by choijunios on 1/29/25.
+//
+
+import UIKit
+
+import SnapKit
+
+final class DockView: UIView {
+    
+    // Sub
+    private var dockLayer = CALayer()
+    
+    override var intrinsicContentSize: CGSize {
+        .init(width: UIView.noIntrinsicMetric, height: 17)
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        setupUI()
+    }
+    required init?(coder: NSCoder) { nil }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let viewSize = self.layer.bounds.size
+        let dockSize = CGSize(width: 36,height: 5)
+        dockLayer.frame = .init(
+            origin: .init(
+                x: (viewSize.width-dockSize.width)/2,
+                y: (viewSize.height-dockSize.height)/2
+            ),
+            size: dockSize
+        )
+        dockLayer.cornerRadius = dockSize.height/2
+    }
+    
+    
+    private func setupUI() {
+        
+        // self
+        self.backgroundColor = .clear
+        
+        
+        // dockLayer
+        dockLayer.backgroundColor = UIColor.gray.cgColor
+        self.layer.addSublayer(dockLayer)
+    }
+}
