8060b75940ec18aa20b2d90edf1cd58c16b0d68b
J0onYEong <cjy2134@naver.com>
2025-02-01 16:24:50 +0900

    Feat, RO프로퍼티를 퍼블릭으로 변경

--- Projects/Feature/Main/Feature/Sources/Model/AlarmCellRO.swift
+++ Projects/Feature/Main/Feature/Sources/Model/AlarmCellRO.swift
@@ -11,13 +11,20 @@ struct AlarmCellRO: Identifiable {
     
     let id: String
     
-    let iterationType: AlarmIterationType
-    let meridiem: Meridiem
-    let hour: Int
-    let minute: Int?
+    var iterationType: AlarmIterationType
+    var meridiem: Meridiem
+    var hour: Int
+    var minute: Int?
     var isActive: Bool
     
-    init(id: String = UUID().uuidString,iterationType: AlarmIterationType, meridiem: Meridiem, hour: Int, minute: Int?, isActive: Bool) {
+    init(
+        id: String = UUID().uuidString,
+        iterationType: AlarmIterationType,
+        meridiem: Meridiem,
+        hour: Int,
+        minute: Int?,
+        isActive: Bool
+    ) {
         self.id = id
         self.iterationType = iterationType
         self.meridiem = meridiem
