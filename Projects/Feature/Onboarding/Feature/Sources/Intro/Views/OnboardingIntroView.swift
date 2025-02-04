0640ec8ff7abc815251ede800fbe206fd7b69229
ByeonggeunSon <qudrms007@naver.com>
2025-02-05 07:40:24 +0900

    Fix: 온보딩 첫화면 버튼 하단 간격 추가

--- Projects/Feature/Onboarding/Feature/Sources/Intro/Views/OnboardingIntroView.swift
+++ Projects/Feature/Onboarding/Feature/Sources/Intro/Views/OnboardingIntroView.swift
@@ -99,7 +99,7 @@ private extension OnboardingIntroView {
         }
         
         nextButton.snp.makeConstraints {
-            $0.bottom.equalTo(safeAreaLayoutGuide)
+            $0.bottom.equalTo(safeAreaLayoutGuide).offset(-12)
             $0.horizontalEdges.equalToSuperview().inset(20)
         }
     }
