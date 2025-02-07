212d160d84d14f73f47f24bb6d314b9db138fe1c
ByeonggeunSon <qudrms007@naver.com>
2025-02-08 06:02:05 +0900

    Feat: 건강, 애정운 화면 추가, 디자인 리소스 추가, 정렬 방식 변경

--- Projects/Feature/Fortune/Feature/Sources/FortuneHealthLoveView.swift
+++ Projects/Feature/Fortune/Feature/Sources/FortuneHealthLoveView.swift
@@ -1,5 +1,5 @@
 //
-//  FortuneStudyMoneyView.swift
+//  FortuneHealthLoveView.swift
 //  FeatureFortune
 //
 //  Created by ever on 2/8/25.
@@ -9,7 +9,7 @@ import UIKit
 import FeatureUIDependencies
 import FeatureThirdPartyDependencies
 
-final class FortuneStudyMoneyView: UIView {
+final class FortuneHealthLoveView: UIView {
     init() {
         super.init(frame: .zero)
         setupUI()
@@ -27,25 +27,25 @@ final class FortuneStudyMoneyView: UIView {
     private let titleLabel = UILabel()
     private let paperContainer = UIImageView()
     private let contentStackView = UIStackView()
-    private let studyContentView = TodayFortuneContentView(
+    private let healthContentView = TodayFortuneContentView(
         icon: FeatureResourcesAsset.svgIcoFortuneStudy.image,
-        title: "학업/직장운 88점",
-        titleColor: R.Color.letterBlue2,
+        title: "건강운 95점",
+        titleColor: R.Color.letterGreen,
         content: """
-        오늘은 집중력이 최고조야! 머릿속에 있는 생각들을 정리하고 효율적으로 공부하거나 업무에 집중하면 좋은 결과를 얻을 수 있을 거야. 복잡한 문제는 차근차근 풀어나가면 답을 찾을 수 있을 거야. 잠깐의 휴식도 잊지 말고 틈틈이 스트레칭을 하면서 긴장을 풀어주자!
+        오늘은 컨디션 최고! 몸이 가볍고 활력이 넘칠 거야. 평소보다 운동을 좀 더 해보거나, 건강한 음식을 챙겨 먹으면 더욱 좋을 거야. 충분한 수면으로 컨디션을 유지하는 것도 잊지 말고!
         """
     )
-    private let moneyContentView = TodayFortuneContentView(
+    private let loveContentView = TodayFortuneContentView(
         icon: FeatureResourcesAsset.svgIcoFortuneMoney.image,
-        title: "재물운 72점",
-        titleColor: R.Color.letterBabyPink,
+        title: "애정운 67점",
+        titleColor: R.Color.letterPink,
         content: """
-        오늘은 투자보다는 안전한 자산 관리에 집중하는 게 좋아. 계획에 없던 지출은 피하고, 예산을 꼼꼼하게 관리하면 재정적으로 안정적인 하루를 보낼 수 있을 거야. 쓸데없는 소비를 줄여서 저축을 늘려보는 건 어때?
+        솔로라면 새로운 만남의 기회가 있을 수 있어. 적극적으로 다가가 보는 것도 좋을 것 같아! 커플이라면 서로의 마음을 확인하는 시간을 가져보는 건 어때? 작은 선물이나 편지를 통해 애정을 표현해보는 것도 좋은 방법일 거야.
         """
     )
 }
 
-private extension FortuneStudyMoneyView {
+private extension FortuneHealthLoveView {
     func setupUI() {
         backgroundColor = .init(red: 72/255, green: 145/255, blue: 240/255, alpha: 1)
         backgroundImageView.do {
@@ -84,7 +84,7 @@ private extension FortuneStudyMoneyView {
             addSubview($0)
         }
         paperContainer.addSubview(contentStackView)
-        [studyContentView, moneyContentView].forEach {
+        [healthContentView, loveContentView].forEach {
             contentStackView.addArrangedSubview($0)
         }
         
