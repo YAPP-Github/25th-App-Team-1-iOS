1d7af5b2d853f61f568def51cb8e125e0d45efc0
ByeonggeunSon <qudrms007@naver.com>
2025-02-08 12:19:25 +0900

    Feat: 푸시 클릭 시 알람 재생 중지하도록 함.

--- Projects/Orbit/Sources/AppDelegate.swift
+++ Projects/Orbit/Sources/AppDelegate.swift
@@ -4,6 +4,8 @@
 
 import UIKit
 import RIBs
+import UserNotifications
+import FeatureCommonDependencies
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,9 +19,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.launchRouter = launchRouter
         launchRouter.launch(from: window)
         self.window = window
+        
+        UNUserNotificationCenter.current().delegate = self
         return true
     }
     
     private var launchRouter: LaunchRouting?
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        // 알림의 userInfo나 actionIdentifier를 통해 원하는 분기 처리 가능
+        if response.actionIdentifier == UNNotificationDefaultActionIdentifier {
+            AlarmManager.shared.stopPlayingSound()
+        }
+        completionHandler()
+    }
+}
