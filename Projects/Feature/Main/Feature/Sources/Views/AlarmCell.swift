39e4b410fa087eb2ef3e5b11b482f23e499d4d41
J0onYEong <cjy2134@naver.com>
2025-04-02 21:38:41 +0900

    Feat, AlarmRowView구현

--- Projects/Feature/Main/Feature/Sources/Views/AlarmCell.swift
+++ Projects/Feature/Main/Feature/Sources/Views/AlarmCell.swift
@@ -15,18 +15,14 @@ import FeatureCommonDependencies
 import RxSwift
 import RxRelay
 
-final class AlarmCell: UITableViewCell {
+final class AlarmCell: UITableViewCell, AlarmRowViewListener {
     
     // Id
     static let identifier = String(describing: AlarmCell.self)
     
     
     // Action
-    enum Action {
-        case activityToggleTapped
-        case cellIsLongPressed
-        case cellIsTapped
-    }
+    typealias Action = AlarmRowView.Action
     
     
     // Listener
@@ -38,255 +34,53 @@ final class AlarmCell: UITableViewCell {
     private let tapGesture: UITapGestureRecognizer = .init()
     
     
-    // Suv view
-    // - Label
-    private let everyWeekLabel: UILabel = .init()
-    private let dayLabel: UILabel = .init()
-    private let dayLabelStack: UIStackView = .init().then {
-        $0.axis = .horizontal
-        $0.alignment = .center
-        $0.spacing = 4
-    }
-    private let holidayImage: UIImageView = .init()
-    private let meridiemLabel: UILabel = .init()
-    private let hourAndMinuteLabel: UILabel = .init()
-    private let clockLabelStack: UIStackView = .init().then {
-        $0.axis = .horizontal
-        $0.spacing = 6
-    }
-    private let timeLabelContainer: UIStackView = .init().then {
-        $0.axis = .vertical
-        $0.spacing = 4
-        $0.alignment = .leading
-    }
-    // - Delete selection
-    private let checkBox: DSCheckBox = .init(initialState: .idle, buttonStyle: .init(size: .medium))
-    private var checkBoxRightConstraint: Constraint?
+    // Sub view
+    private let alarmRowView = AlarmRowView()
     
-    private let toggle: DSToggle = .init(initialState: .init(isEnabled: true, switchState: .off))
-    
-    private let containerView: UIStackView = .init().then {
-        $0.axis = .horizontal
-        $0.distribution = .fill
-        $0.alignment = .center
-    }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupUI()
         setupLayout()
-        setupGesture()
     }
     required init?(coder: NSCoder) { nil }
-}
-
-
-// MARK: Setup
-private extension AlarmCell {
-    func setupUI() {
-        
-        // contentView
-        contentView.backgroundColor = R.Color.gray900
-        
-        
-        // everyWeekLabel
-        everyWeekLabel.displayText = "매주".displayText(
-            font: .label1SemiBold,
-            color: R.Color.gray300
-        )
-        
-        
-        // holidayImage
-        holidayImage.image = FeatureResourcesAsset.holiday.image
-        holidayImage.contentMode = .scaleAspectFit
-        holidayImage.tintColor = R.Color.gray300
-        
-        
-        // dayLabelStack
-        [everyWeekLabel, dayLabel, holidayImage, UIView()].forEach {
-            dayLabelStack.addArrangedSubview($0)
-        }
-        
-        
-        // clockLabelStack
-        [meridiemLabel, hourAndMinuteLabel, UIView()].forEach {
-            clockLabelStack.addArrangedSubview($0)
-        }
-        
-        
-        // timeLabelContainer
-        [dayLabelStack, clockLabelStack].forEach {
-            timeLabelContainer.addArrangedSubview($0)
-        }
-        
-        
-        // checkBox
-        checkBox.isHidden = true
-        contentView.addSubview(checkBox)
-        
-        
-        // Toggle view
-        toggle.toggleAction = { [weak self] in
-            guard let self else { return }
-            action?(.activityToggleTapped)
-        }
-        
-        
-        // containerView
-        [timeLabelContainer, UIView(), toggle].forEach {
-            containerView.addArrangedSubview($0)
-        }
-        contentView.addSubview(containerView)
-    }
-    
-    func setupLayout() {
-        
-        // holidayImage
-        holidayImage.snp.makeConstraints { make in
-            make.width.equalTo(12)
-            make.height.equalTo(12)
-        }
-        
-        
-        // dayLabelStack
-        dayLabelStack.snp.makeConstraints { make in
-            make.height.equalTo(20)
-        }
-        
-        
-        // containerView
-        containerView.snp.makeConstraints { make in
-            make.centerY.equalToSuperview()
-            make.horizontalEdges.equalToSuperview().inset(22).priority(.high)
-        }
-        
-        
-        // checkBox
-        checkBox.snp.makeConstraints { make in
-            make.left.equalToSuperview().inset(24)
-            make.centerY.equalToSuperview()
-            self.checkBoxRightConstraint = make.right.equalTo(containerView.snp.left).offset(-12).constraint
-        }
-        checkBoxRightConstraint?.deactivate()
-    }
-    
-    func setupGesture() {
-        // longPressGesture
-        contentView.addGestureRecognizer(longPressGesture)
-        longPressGesture.delegate = self
-        longPressGesture.addTarget(self, action: #selector(onLongPress(_:)))
-        longPressGesture.minimumPressDuration = 0.5
-        longPressGesture.cancelsTouchesInView = false
-        
-        // tapGesture
-        contentView.addGestureRecognizer(tapGesture)
-        tapGesture.delegate = self
-        tapGesture.require(toFail: longPressGesture)
-        tapGesture.addTarget(self, action: #selector(onTap(_:)))
-        tapGesture.cancelsTouchesInView = false
-    }
-    @objc
-    func onTap(_ sender: UITapGestureRecognizer) {
-        action?(.cellIsTapped)
-    }
-    @objc
-    func onLongPress(_ sender: UILongPressGestureRecognizer) {
-        if sender.state == .began {
-            action?(.cellIsLongPressed)
-        }
-    }
 }
 
 
 // MARK: Public interface
 extension AlarmCell {
     @discardableResult
-    func update(renderObject ro: AlarmCellRO, animated: Bool = true) -> Self {
-        let isAlarmActive = ro.isToggleOn
-        
-        // Toggle
-        toggle.update(state: .init(
-            isEnabled: true,
-            switchState: isAlarmActive ? .on : .off
-        ), animated: animated)
-        
-        
-        // day
-        let dayColor = isAlarmActive ? R.Color.gray300 : R.Color.gray500
-        let alarmDays = ro.alarmDays
-        everyWeekLabel.isHidden = alarmDays.days.isEmpty
-        holidayImage.isHidden = !alarmDays.shoundTurnOffHolidayAlarm
-        everyWeekLabel.displayText = everyWeekLabel.displayText?.string.displayText(
-            font: .label1SemiBold,
-            color: dayColor
-        )
-        var dayDisplayText = ""
-        if !alarmDays.days.isEmpty {
-            dayDisplayText = alarmDays.days
-                .sorted(by: { $0.rawValue < $1.rawValue })
-                .map { $0.toShortKoreanFormat }.joined(separator: ", ")
-        } else {
-            var alarmHour = ro.hour.value
-            if ro.meridiem == .pm, (1...11).contains(alarmHour) {
-                alarmHour += 12
-            }
-            let alarmMinute = ro.minute.value
-            let currentHour = Calendar.current.component(.hour, from: .now)
-            let currentMinute = Calendar.current.component(.minute, from: .now)
-            
-            // 알람이 현재 시간 이후에 울리는지 확인
-            let isTodayAlarm = (alarmHour > currentHour) || (alarmHour == currentHour && alarmMinute > currentMinute)
-            var alarmDate: Date = .now
-            if !isTodayAlarm {
-                // 내일 울릴 알람인 경우
-                alarmDate = Calendar.current.date(byAdding: .day, value: 1, to: .now)!
-            }
-            let month = Calendar.current.component(.month, from: alarmDate)
-            let day = Calendar.current.component(.day, from: alarmDate)
-            dayDisplayText = "\(month)월 \(day)일"
-        }
-        dayLabel.displayText = dayDisplayText.displayText(
-            font: .label1SemiBold,
-            color: dayColor
-        )
-        holidayImage.tintColor = dayColor
-        
-        // clock
-        let clockColor = isAlarmActive ? R.Color.white100 : R.Color.gray500
-        meridiemLabel.displayText = ro.meridiem.toKoreanFormat.displayText(
-            font: .title2Medium,
-            color: clockColor
-        )
-        hourAndMinuteLabel.displayText = String(format: "%d:%02d", ro.hour.value, ro.minute.value).displayText(
-            font: .title2Medium,
-            color: clockColor
-        )
-        
-        // Mode
-        switch ro.mode {
-        case .idle:
-            checkBox.isHidden = true
-            toggle.isHidden = false
-            checkBoxRightConstraint?.deactivate()
-            contentView.backgroundColor = R.Color.gray900
-        case .deletion:
-            checkBox.isHidden = false
-            toggle.isHidden = true
-            checkBoxRightConstraint?.activate()
-            checkBox.update(state: ro.isChecked ? .seleceted : .idle)
-            contentView.backgroundColor = ro.isChecked ? R.Color.gray800 : R.Color.gray900
-        }
+    func update(renderObject: AlarmCellRO, animated: Bool = true) -> Self {
+        alarmRowView.update(renderObject: renderObject, animated: animated)
         return self
     }
 }
 
 
-// MARK: UIGestureRecognizerDelegate
-extension AlarmCell {
-    override func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
-        if touch.view === toggle {
-            return false
+// MARK: Set up
+private extension AlarmCell {
+    func setupUI() {
+        // contentView
+        contentView.backgroundColor = .clear
+        
+        
+        // alarmRowView
+        alarmRowView.listener = self
+        contentView.addSubview(alarmRowView)
+    }
+    
+    func setupLayout() {
+        // alarmRowView
+        alarmRowView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
         }
-        return true
+    }
+}
+
+
+// MARK: AlarmRowViewListener
+extension AlarmCell {
+    func action(_ action: AlarmRowView.Action) {
+        self.action?(action)
     }
 }
---
+++ Projects/Feature/Main/Feature/Sources/Views/AlarmRowView.swift
@@ -0,0 +1,291 @@
+//
+//  AlarmRowView.swift
+//  Main
+//
+//  Created by choijunios on 4/2/25.
+//
+
+import UIKit
+
+import FeatureResources
+import FeatureDesignSystem
+import FeatureThirdPartyDependencies
+import FeatureCommonDependencies
+
+import RxSwift
+import RxRelay
+
+protocol AlarmRowViewListener: AnyObject {
+    func action(_ action: AlarmRowView.Action)
+}
+
+final class AlarmRowView: UIView, UIGestureRecognizerDelegate {
+    
+    // Action
+    enum Action {
+        case activityToggleTapped
+        case cellIsLongPressed
+        case cellIsTapped
+    }
+    
+    // Listener
+    weak var listener: AlarmRowViewListener?
+    
+    
+    // Gesture
+    private let longPressGesture: UILongPressGestureRecognizer = .init()
+    private let tapGesture: UITapGestureRecognizer = .init()
+    
+    
+    // Suv view
+    // - Label
+    private let everyWeekLabel: UILabel = .init()
+    private let dayLabel: UILabel = .init()
+    private let dayLabelStack: UIStackView = .init().then {
+        $0.axis = .horizontal
+        $0.alignment = .center
+        $0.spacing = 4
+    }
+    private let holidayImage: UIImageView = .init()
+    private let meridiemLabel: UILabel = .init()
+    private let hourAndMinuteLabel: UILabel = .init()
+    private let clockLabelStack: UIStackView = .init().then {
+        $0.axis = .horizontal
+        $0.spacing = 6
+    }
+    private let timeLabelContainer: UIStackView = .init().then {
+        $0.axis = .vertical
+        $0.spacing = 4
+        $0.alignment = .leading
+    }
+    // - Delete selection
+    private let checkBox: DSCheckBox = .init(initialState: .idle, buttonStyle: .init(size: .medium))
+    private var checkBoxRightConstraint: Constraint?
+    
+    private let toggle: DSToggle = .init(initialState: .init(isEnabled: true, switchState: .off))
+    
+    private let containerView: UIStackView = .init().then {
+        $0.axis = .horizontal
+        $0.distribution = .fill
+        $0.alignment = .center
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        setupUI()
+        setupLayout()
+        setupGesture()
+    }
+    required init?(coder: NSCoder) { nil }
+}
+
+
+// MARK: Setup
+private extension AlarmRowView {
+    func setupUI() {
+        
+        // contentView
+        self.backgroundColor = R.Color.gray900
+        
+        
+        // everyWeekLabel
+        everyWeekLabel.displayText = "매주".displayText(
+            font: .label1SemiBold,
+            color: R.Color.gray300
+        )
+        
+        
+        // holidayImage
+        holidayImage.image = FeatureResourcesAsset.holiday.image
+        holidayImage.contentMode = .scaleAspectFit
+        holidayImage.tintColor = R.Color.gray300
+        
+        
+        // dayLabelStack
+        [everyWeekLabel, dayLabel, holidayImage, UIView()].forEach {
+            dayLabelStack.addArrangedSubview($0)
+        }
+        
+        
+        // clockLabelStack
+        [meridiemLabel, hourAndMinuteLabel, UIView()].forEach {
+            clockLabelStack.addArrangedSubview($0)
+        }
+        
+        
+        // timeLabelContainer
+        [dayLabelStack, clockLabelStack].forEach {
+            timeLabelContainer.addArrangedSubview($0)
+        }
+        
+        
+        // checkBox
+        checkBox.isHidden = true
+        self.addSubview(checkBox)
+        
+        
+        // Toggle view
+        toggle.toggleAction = { [weak self] in
+            guard let self else { return }
+            listener?.action(.activityToggleTapped)
+        }
+        
+        
+        // containerView
+        [timeLabelContainer, UIView(), toggle].forEach {
+            containerView.addArrangedSubview($0)
+        }
+        self.addSubview(containerView)
+    }
+    
+    func setupLayout() {
+        
+        // holidayImage
+        holidayImage.snp.makeConstraints { make in
+            make.width.equalTo(12)
+            make.height.equalTo(12)
+        }
+        
+        
+        // dayLabelStack
+        dayLabelStack.snp.makeConstraints { make in
+            make.height.equalTo(20)
+        }
+        
+        
+        // containerView
+        containerView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.horizontalEdges.equalToSuperview().inset(22).priority(.high)
+        }
+        
+        
+        // checkBox
+        checkBox.snp.makeConstraints { make in
+            make.left.equalToSuperview().inset(24)
+            make.centerY.equalToSuperview()
+            self.checkBoxRightConstraint = make.right.equalTo(containerView.snp.left).offset(-12).constraint
+        }
+        checkBoxRightConstraint?.deactivate()
+    }
+    
+    func setupGesture() {
+        // longPressGesture
+        self.addGestureRecognizer(longPressGesture)
+        longPressGesture.delegate = self
+        longPressGesture.addTarget(self, action: #selector(onLongPress(_:)))
+        longPressGesture.minimumPressDuration = 0.5
+        longPressGesture.cancelsTouchesInView = false
+        
+        // tapGesture
+        self.addGestureRecognizer(tapGesture)
+        tapGesture.delegate = self
+        tapGesture.require(toFail: longPressGesture)
+        tapGesture.addTarget(self, action: #selector(onTap(_:)))
+        tapGesture.cancelsTouchesInView = false
+    }
+    @objc
+    func onTap(_ sender: UITapGestureRecognizer) {
+        listener?.action(.cellIsTapped)
+    }
+    @objc
+    func onLongPress(_ sender: UILongPressGestureRecognizer) {
+        if sender.state == .began {
+            listener?.action(.cellIsLongPressed)
+        }
+    }
+}
+
+
+// MARK: Public interface
+extension AlarmRowView {
+    @discardableResult
+    func update(renderObject ro: AlarmCellRO, animated: Bool = true) -> Self {
+        let isAlarmActive = ro.isToggleOn
+        
+        // Toggle
+        toggle.update(state: .init(
+            isEnabled: true,
+            switchState: isAlarmActive ? .on : .off
+        ), animated: animated)
+        
+        
+        // day
+        let dayColor = isAlarmActive ? R.Color.gray300 : R.Color.gray500
+        let alarmDays = ro.alarmDays
+        everyWeekLabel.isHidden = alarmDays.days.isEmpty
+        holidayImage.isHidden = !alarmDays.shoundTurnOffHolidayAlarm
+        everyWeekLabel.displayText = everyWeekLabel.displayText?.string.displayText(
+            font: .label1SemiBold,
+            color: dayColor
+        )
+        var dayDisplayText = ""
+        if !alarmDays.days.isEmpty {
+            dayDisplayText = alarmDays.days
+                .sorted(by: { $0.rawValue < $1.rawValue })
+                .map { $0.toShortKoreanFormat }.joined(separator: ", ")
+        } else {
+            var alarmHour = ro.hour.value
+            if ro.meridiem == .pm, (1...11).contains(alarmHour) {
+                alarmHour += 12
+            }
+            let alarmMinute = ro.minute.value
+            let currentHour = Calendar.current.component(.hour, from: .now)
+            let currentMinute = Calendar.current.component(.minute, from: .now)
+            
+            // 알람이 현재 시간 이후에 울리는지 확인
+            let isTodayAlarm = (alarmHour > currentHour) || (alarmHour == currentHour && alarmMinute > currentMinute)
+            var alarmDate: Date = .now
+            if !isTodayAlarm {
+                // 내일 울릴 알람인 경우
+                alarmDate = Calendar.current.date(byAdding: .day, value: 1, to: .now)!
+            }
+            let month = Calendar.current.component(.month, from: alarmDate)
+            let day = Calendar.current.component(.day, from: alarmDate)
+            dayDisplayText = "\(month)월 \(day)일"
+        }
+        dayLabel.displayText = dayDisplayText.displayText(
+            font: .label1SemiBold,
+            color: dayColor
+        )
+        holidayImage.tintColor = dayColor
+        
+        // clock
+        let clockColor = isAlarmActive ? R.Color.white100 : R.Color.gray500
+        meridiemLabel.displayText = ro.meridiem.toKoreanFormat.displayText(
+            font: .title2Medium,
+            color: clockColor
+        )
+        hourAndMinuteLabel.displayText = String(format: "%d:%02d", ro.hour.value, ro.minute.value).displayText(
+            font: .title2Medium,
+            color: clockColor
+        )
+        
+        // Mode
+        switch ro.mode {
+        case .idle:
+            checkBox.isHidden = true
+            toggle.isHidden = false
+            checkBoxRightConstraint?.deactivate()
+            self.backgroundColor = R.Color.gray900
+        case .deletion:
+            checkBox.isHidden = false
+            toggle.isHidden = true
+            checkBoxRightConstraint?.activate()
+            checkBox.update(state: ro.isChecked ? .seleceted : .idle)
+            self.backgroundColor = ro.isChecked ? R.Color.gray800 : R.Color.gray900
+        }
+        return self
+    }
+}
+
+
+// MARK: UIGestureRecognizerDelegate
+extension AlarmRowView {
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        if touch.view === toggle {
+            return false
+        }
+        return true
+    }
+}
