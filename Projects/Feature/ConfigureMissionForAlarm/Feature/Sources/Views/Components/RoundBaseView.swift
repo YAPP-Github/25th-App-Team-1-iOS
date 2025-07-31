6c5a0b00c44d31a7b9af2847d4f5a1f36bb73f33
J0onYEong <cjy2134@naver.com>
2025-07-31 12:31:42 +0900

    Feat, RoundBaseView구현

---
+++ Projects/Feature/ConfigureMissionForAlarm/Feature/Sources/Views/Components/RoundBaseView.swift
@@ -0,0 +1,48 @@
+//
+//  RoundBaseView.swift
+//  ConfigureMissionForAlarm
+//
+//  Created by choijunios on 7/30/25.
+//
+
+import UIKit
+
+import FeatureResources
+import FeatureThirdPartyDependencies
+
+final class RoundBaseView: UIView {
+    
+    private var contentsView: UIView?
+    
+    init() {
+        super.init(frame: .zero)
+    }
+    required init?(coder: NSCoder) { nil }
+    
+    func change(contentsView: UIView, animated: Bool = false) {
+        
+        UIView.animate(withDuration: animated ? 0.3 : 0) {
+            
+            if let prevView = self.contentsView {
+                prevView.removeFromSuperview()
+            }
+            
+            self.addSubview(contentsView)
+            self.contentsView = contentsView
+            contentsView.snp.makeConstraints { make in
+                make.edges.equalToSuperview()
+            }
+            
+            if animated { self.layoutIfNeeded() }
+        }
+    }
+    
+    private func setupUI() {
+        self.backgroundColor = R.Color.gray800
+        self.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        self.layer.borderColor = R.Color.gray700.cgColor
+        self.layer.borderWidth = 1
+        self.layer.cornerRadius = 28
+        self.layer.masksToBounds = true
+    }
+}
